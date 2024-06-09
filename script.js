89a7aa6dea95783b5e0eeb23b0c9b07977ba1ff6
Mariia <masha.tzimbal@gmail.com>
2024-06-09 18:07:50 +0300

    Full resume adding

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Add event listener to each toggle button
 document.querySelectorAll('.toggle-button').forEach((button) => {
-    button.addEventListener('click', () => {
-        const extendedResume = button.nextElementSibling;
+    button.addEventListener('click', (event) => {
+        const resume = event.target.parentNode;
+        const extendedResume = resume.querySelector('.extended-resume');
         extendedResume.style.display = extendedResume.style.display === 'block'? 'none' : 'block';
+        
+        // Close other extended resumes
+        document.querySelectorAll('.extended-resume').forEach((otherExtendedResume) => {
+            if (otherExtendedResume!== extendedResume) {
+                otherExtendedResume.style.display = 'none';
+            }
+        });
     });
 });
